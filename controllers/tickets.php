5d4cf84db82cfe2346a6688594d8edf8eb5dbd5f
samliweisen <liweisen007@gmail.com>
2018-04-02 17:50:01 +0000

    add get all tickets and upsert ticket function

--- controllers/tickets.php
+++ controllers/tickets.php
@@ -8,3 +8,16 @@ $ticketRepo = new Item(Database::dbConnect());
 header('Content-Type: application/json');
 
 $action = $_GET['action'];
+
+switch ($action) {
+    case 'getAll':
+        $tickets = $ticketRepo->tickets();
+        echo json_encode($tickets);
+        break;
+    case 'updateTicket':
+        $ticket = $ticketRepo->updateTicket($_POST);
+        echo json_encode($ticket);
+    default:
+        // code...
+        break;
+}
